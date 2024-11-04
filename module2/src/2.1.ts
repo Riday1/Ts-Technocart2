9072a04816672ab47a96f180a155b5fd087aeac5
Riday1 <rrriday100@gmail.com>
2024-11-04 21:58:26 +0600

    type assertion 2

--- module2/src/2.1.ts
+++ module2/src/2.1.ts
@@ -1,8 +1,23 @@
 {
-    //
-    
+    // Type Assertion
+
+    let anything: any;
+    anything = 'riday';
+    // (anything as string).
 
 
+    const kgToGram = (value: string | number): string | number | undefined => {
+        if (typeof value === 'string') {
+            const convertedValue = parseFloat(value) * 1000
+            return `The converted value is :${convertedValue}`
+        }
+        if (typeof value === 'number') {
+            return value * 1000
+        }
+    }
 
+
+    const result1 = kgToGram(1000) as number
+    const result2 = kgToGram('1000') as string
     //
 }
