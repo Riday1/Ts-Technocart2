e2b518923528da5d6ec5bc657ca4e171d66e1785
Riday1 <rrriday100@gmail.com>
2024-11-07 18:16:41 +0600

    constraint with keyof

---
+++ module2/src/2.7.ts
@@ -0,0 +1,31 @@
+{
+    // "Generic constraint" with "keyof" operator
+
+
+    type vehicle = {
+        bike: string;
+        car: string;
+        ship: string
+    }
+
+    type Owner = "bike" | "car" | "ship" // manually
+
+    type Owner2 = keyof vehicle // vechicle er key gulo niye ekta union type gothon korba
+
+    const user = {
+        name: "mr x",
+        age: 21,
+        address: 'ctg'
+    }
+
+    const getPropertyValue = <X, Y extends keyof X>(obj: X, key: Y) => {
+        return obj[key]
+    }
+
+    getPropertyValue(user, "name")
+
+
+
+
+    //
+}
